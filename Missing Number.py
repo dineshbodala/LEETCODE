6d4ea0fcdcd445e66e54fef5ad8e1cc192b88153
Dinesh Bodala <ds176.edu@gmail.com>
2022-08-15 22:36:50 +0530

    14th Aug 2022

---
+++ Missing Number.py
@@ -0,0 +1,13 @@
+#Missing Number
+
+#Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ 
+#Basic sum of n natural numbers formula
+class Solution(object):
+    def missingNumber(self, nums):
+        return (len(nums))*(len(nums)+1)//2 - sum(nums)
+
+
+        
+        
+         
